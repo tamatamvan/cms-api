e4abb57cef56ef69f2e3de5f1a0b36c38760228c
Septian A Tama <sfirst.tama@gmail.com>
2016-11-17 13:57:59 +0700

    controller and routes for datadate

--- server/controllers/dataDateController.js
+++ server/controllers/dataDateController.js
@@ -1,28 +1,76 @@
-const DataDates = require('./models/DataDates');
+const DataDates = require('../models/DataDates');
 
 let getAllDataDate  = (req, res, next) => {
-
+  DataDates.find({}, (err, datas) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(datas);
+    }
+  })
 }
-let getDataDateByLetter  = (req, res, next) => {
 
+let getDataDateByDate  = (req, res, next) => {
+  DataDates.find({
+    date: req.params.date
+  }, (err, datas) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(datas);
+    }
+  })
 }
 let getDataDateByFreq  = (req, res, next) => {
-
+  DataDates.find({
+    frequency: req.params.frequency
+  }, (err, datas) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(datas);
+    }
+  })
 }
 let createNewDataDate  = (req, res, next) => {
-
+  DataDates.create({
+    letter: req.body.letter,
+    frequency: req.body.frequency
+  }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  })
 }
 let updateDataDateById  = (req, res, next) => {
-
+  DataDates.update({
+    _id: req.params.id
+  }, req.body, (err, updated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(updated);
+    }
+  })
 }
 let deleteDataDateById  = (req, res, next) => {
-
+  DataDates.remove({
+    _id: req.params.id
+  }, (err, deleted) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(deleted);
+    }
+  })
 }
 
 module.exports = {
   getAllDataDate,
   getDataDateByFreq,
-  getDataDateByLetter,
+  getDataDateByDate,
   createNewDataDate,
   updateDataDateById,
   deleteDataDateById
--- server/routes/apiDataDate.js
+++ server/routes/apiDataDate.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const dataDateController = require('../controllers/dataDateController');
 
 /* GET users listing. */
-router.get('/', );
-router.get('/search_date/:date');
-router.get('/search_frequency/:freq');
-router.post('/');
-router.put('/:id', );
-router.delete('/:id',);
+router.get('/', dataDateController.getAllDataDate);
+router.get('/search_letter/:date', dataDateController.getDataDateByDate);
+router.get('/search_frequency/:freq', dataDateController.getDataDateByFreq);
+router.post('/', dataDateController.createNewDataDate);
+router.put('/:id', dataDateController.updateDataDateById);
+router.delete('/:id', dataDateController.deleteDataDateById);
 
 module.exports = router;
